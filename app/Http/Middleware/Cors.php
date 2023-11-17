ef3e6737ae55a209ee84d6b34052aa8a7a659833
ivargasm <ivargasm@hotmail.com>
2023-11-17 13:25:12 -0600

    ajustes cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,7 @@ class Cors
     {
         return $next($request)
         //Url a la que se le dará acceso en las peticiones
-        ->header("Access-Control-Allow-Origin", "https://studyquizarena.netlify.app/")
+        ->header("Access-Control-Allow-Origin", "*")
         //Métodos que a los que se da acceso
         ->header("Access-Control-Allow-Methods", "GET")
         //Headers de la petición
